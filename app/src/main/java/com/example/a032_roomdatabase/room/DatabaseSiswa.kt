0176353f64f51f89ec17ed4c55a81e4226cedb69
Othallia <vianiputriocta6@gmail.com>
2025-11-28 15:33:28 +0700

    menambah kelas DatabaseSiswa sebagai database utama

---
+++ app/src/main/java/com/example/a032_roomdatabase/room/DatabaseSiswa.kt
@@ -0,0 +1,29 @@
+package com.example.a032_roomdatabase.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase() {
+
+    abstract fun siswaDao(): SiswaDao
+
+    companion object {
+        @Volatile
+        private var instance: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    DatabaseSiswa::class.java,
+                    "Siswa_Database"
+                ).build().also {
+                    instance = it
+                }
+            }
+        }
+    }
+}
