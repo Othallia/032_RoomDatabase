c3f57e0f816f431720b16b36748916f4286a97c1
Othallia <vianiputriocta6@gmail.com>
2025-11-28 15:15:00 +0700

    menambah entity Siswa untuk Room

---
+++ app/src/main/java/com/example/a032_roomdatabase/room/Siswa.kt
@@ -0,0 +1,15 @@
+package com.example.a032_roomdatabase.room
+
+import android.R
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "tblSiswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
