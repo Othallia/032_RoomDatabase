5cdfbe59419474eef005c49b2b04f8fb4f2bb105
Othallia <vianiputriocta6@gmail.com>
2025-11-28 17:48:31 +0700

    menambahkan DestinasiHome.kt

---
+++ app/src/main/java/com/example/a032_roomdatabase/view/Route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.a032_roomdatabase.view.route
+
+import com.example.a032_roomdatabase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
